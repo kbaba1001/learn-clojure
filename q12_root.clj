90e68f33bad912a5bb86eb6f4f0b409509a73d41
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-04-29 06:30:27 +0900

    q12 sqrt root

--- q12_root.clj
+++ q12_root.clj
@@ -1,12 +1,18 @@
-(zipmap (range 10) (repeat 10 false))
+(def init-numbers (zipmap (range 10) (repeat 10 false)))
 
 (defn sqrt-seq [n]
-  (map-indexed (fn [idx itm] [idx itm]) (re-seq #"\d" (format "%10.20f" (Math/sqrt n)))))
-; ([0 "1"]
-;  [1 "4"]
-;  [2 "1"]
-;  [3 "4"]
-;  [4 "2"]...
+  (map-indexed (fn [idx itm] [idx itm]) (map read-string (re-seq #"\d" (format "%10.20f" (Math/sqrt n))))))
 
-(for [x (sqrt-seq 2)]
-  (println x))
+(defn counter [n]
+  (let [numbers (atom init-numbers)]
+    (last (for [[index item] (sqrt-seq n)
+                :while (not-every? true? (vals @numbers))]
+            (do
+              (swap! numbers assoc item true)
+              index)))))
+
+(loop [n 2]
+  (if (= (counter n) 10)
+    n
+    (recur (inc n))))
+; (apply min-key counter (range 100))
