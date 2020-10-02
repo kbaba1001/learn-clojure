ed373c9e039087a2fd41e50c0c605fdf3b080168
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-10-02 09:52:51 +0900

    quick sort clj

--- quick_sort3.clj
+++ quick_sort3.clj
@@ -1,52 +1,20 @@
-;; def qsort(data)
-;;   left_stack = [0]
-;;   right_stack = [(data.length - 1)]
-;;   ptr = 1
-;;   while ptr > 0 do
-;;     ptr -= 1
-;;     pleft = left = left_stack[ptr]
-;;     pright = right = right_stack[ptr]
-;;     x = data[(pleft + pright) / 2]
-;;     x1 = data[pleft]
-;;     x2 = data[pright]
-;;     if (x <= x1 && x1 <= x2) || (x2 <= x1 && x1 <= x)
-;;       x = x1
-;;     end
-;;     if (x <= x2 && x2 <= x1)||(x1 <= x2 && x2 <= x)
-;;       x = x2
-;;     end
-;;     loop do
-;;       while data[pleft] < x do
-;;         pleft += 1
-;;       end
-;;       while data[pright] > x do
-;;         pright -= 1
-;;       end
-;;       if (pleft <= pright)
-;;         tmp = data[pleft]
-;;         data[pleft] = data[pright]
-;;         data[pright] = tmp
-;;         pleft += 1
-;;         pright -= 1
-;;       end
+(defn qsort [arr]
+  (let [stack (new java.util.Stack)]
+    (with-local-vars [result []]
+      (.push stack arr)
+      (while (not (.empty stack))
+        (let [target (.pop stack)
+              pivot (first target)
+              res (next target)
+              left (filter #(< % pivot) res)
+              right (filter #(>= % pivot) res)]
+          (if (and (empty? left) (empty? right))
+            (var-set result (cons pivot @result))
+            (do
+              (.push stack left)
+              (.push stack [pivot])
+              (.push stack right)))))
+      (remove nil? @result))))
 
-;;       break if pleft <= right
-;;     end
-;;     if (left < pright)
-;;       left_stack[ptr] = left
-;;       right_stack[ptr] = pright
-;;       ptr += 1
-;;     end
-;;     if (pleft < right)
-;;       left_stack[ptr] = pleft
-;;       right_stack[ptr] = right
-;;       ptr += 1
-;;     end
-;;   end
-
-;;   data
-;; end
-
-;; p qsort([3,2,5,4,1,2])
-;; # 入力値: [3,2,5,4,1,2]
-;; # 出力値: [1,2,2,3,4,5]
+(qsort [3,2,5,4,1,2])
+;; => (1 2 2 3 4 5)
