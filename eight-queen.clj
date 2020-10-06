3e4ee637a2ce43d7939d0ce568f4f13d0615df0b
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-10-06 13:02:30 +0900

    Update eight-queen.clj

--- eight-queen.clj
+++ eight-queen.clj
@@ -1,14 +1,6 @@
 (ns eight-queen
   (:require [clojure.string :as s]))
 
-(defn permutations [s]
-  (lazy-seq
-   (if (next s)
-     (for [head s
-           tail (permutations (disj s head))]
-       (cons head tail))
-     [s])))
-
 (defn conflict? [x y board]
   (->> (map-indexed vector (take y board))
        (some (fn [[y1 x1]]
@@ -21,14 +13,15 @@
        (every? (fn [[y x]] (not (conflict? x y board))))))
 
 (defn queen [n]
-  (->> (permutations (set (range n)))
-       (some #(when (check? %) %))))
+  (loop [board (vec (range n))]
+    (if (check? board)
+      board
+      (recur (shuffle board)))))
 
 (defn display-queen [n]
-  (let [board (atom (vec (repeat n (vec (repeat n ".")))))
-        queens (map-indexed vector (queen n))]
-    (doseq [coordinate queens]
-      (reset! board (assoc-in @board coordinate "Q")))
-    (println (s/join "\n" (map #(s/join " " %) @board)))))
+  (let [board (vec (repeat n (vec (repeat n "."))))
+        queens (map-indexed vector (queen n))
+        result (reduce #(assoc-in %1 %2 "Q") board queens)]
+    (println (s/join "\n" (map #(s/join " " %) result)))))
 
 (display-queen 8)
