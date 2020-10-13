4d2180c8f9cdb1d930a85afe9c8ba6cbd9751dba
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-10-13 18:17:17 +0900

    insertion sort and xxx sort

--- sort/insertion_sort.clj
+++ sort/insertion_sort.clj
@@ -1,25 +1,11 @@
 ;; https://en.wikipedia.org/wiki/Insertion_sort
-
-;; ヒープソート?
-;; (defn insertion-sort [coll]
-;;   (reduce
-;;    (fn [r x]
-;;      (let [{left true right false} (group-by #(> x %) r)]
-;;        (concat left right)))
-;;    coll
-;;    coll))
-
 (defn insert [n coll]
   (cond
     (empty? coll) (list n)
-    (> (first coll) n) (conj coll n)
-    :else (conj (insert n (next coll)) (first coll))))
+    (> (first coll) n) (cons n coll)
+    :else (cons (first coll) (insert n (next coll)))))
 
 (defn insertion-sort [coll]
-  (loop [list coll
-         result ()]
-    (if (empty? list)
-      result
-      (recur (next list) (insert (first list) result)))))
+  (reduce #(insert %2 %1) '() coll))
 
 (println (insertion-sort [8 3 1 5 2 1]))
